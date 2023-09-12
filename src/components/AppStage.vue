7c9ddca6e4d34a69863bfe239db9cf37d235f2e1
drinkingtheink <jason.m.harrison@gmail.com>
2023-09-11 23:11:36 -0500

    initial commit

---
+++ src/components/AppStage.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Form Styler</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'AppStage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
